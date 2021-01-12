afa8e68619c7dc1e2faeb2b12a1f5398671c758d
AnatoliyChabanenko <76126324+AnatoliyChabanenko@users.noreply.github.com>
2021-01-12 19:12:35 +0200

    lesson10

--- lesson10_3_2.py
+++ lesson10_3_2.py
@@ -1,39 +1,42 @@
 CHANNELS = ["BBC", "Discovery", "TV1000"]
 
 class TVController:
-    next_number = 0
 
-    def __init__(self, CHANNELS ):
-        self.CHANNELS = CHANNELS
+    def __init__(self, channels):
+        self.channels = channels
+        self.next_number = 0
 
     def first_channel(self):
-        TVController.next_number = 0
-        return self.CHANNELS[self.next_number]
+        self.next_number = 0
+        return self.current_channel()
 
     def last_channel(self):
-        TVController.next_number = -1
-        return self.CHANNELS [self.next_number]\
+        self.next_number = -1
+        return self.current_channel()
 
     def turn_channel(self, N):
-        TVController.next_number= self.N = N - 1
-        return self.CHANNELS[self.N]
+        self.next_number=self.N = N - 1
+        return self.channels[self.N]
 
-    def move_to (self):
-        TVController.next_number += 1
-        if TVController.next_number == 3:
-            return self.CHANNELS[0]
-        return self.CHANNELS [self.next_number]
+    def next_channel (self):
+        if self.next_number != len(self.channels)-1:
+            self.next_number += 1
+        else:
+            self.next_number = 0
+        return self.current_channel()
 
     def previous_channel(self):
-        TVController.next_number -= 1
-        return self.CHANNELS [self.next_number]
+        if self.next_number != 0:
+            self.next_number -= 1
+        else:
+            self.next_number = len(self.channels)-1
 
     def current_channel(self):
-        return self.CHANNELS [self.next_number]
+        return self.channels[self.next_number]
 
     def is_exist(self, name):
         self.name = name
-        if name in self.CHANNELS:
+        if name in self.channels:
             return 'Yes'
         else:
             return 'no'
@@ -42,7 +45,6 @@ controller = TVController(CHANNELS)
 print(controller.first_channel())
 print(controller.last_channel())
 print(controller.turn_channel(1))
-print(controller.move_to())
 print(controller.previous_channel())
 print(controller.current_channel())
 print(controller.is_exist('BBC'))
