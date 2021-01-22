70e3e288e49bdfbfc20e9bc9d013e1cdedb967c4
AnatoliyChabanenko <76126324+AnatoliyChabanenko@users.noreply.github.com>
2021-01-23 00:42:27 +0200

    lesson15

---
+++ lesson15_1.py
@@ -0,0 +1,39 @@
+'''Створіть метод класу з іменем `validate`,
+який слід викликати з методу` __init__` для перевірки
+електронної пошти параметра, переданого конструктору.
+Логіка методу `перевірка` може полягати в тому, щоб перевірити,
+ чи переданий параметр електронної пошти є дійсним рядком
+  електронної пошти.
+'''
+import re
+
+class Email:
+    def __init__(self, email):
+        self.__email = email
+
+    @property
+    def email(self):
+        print('geter call')
+        return self.__email
+
+    @staticmethod
+    def is_include_sobaka(email):
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            return True
+        return False
+
+    @email.setter
+    def email(self, value):
+        print('seter call')
+        if not isinstance(value, str):
+            raise TypeError('емеил должен быть строкой ')
+        if len(value) < 5:
+            raise ValueError('что-то мутишь')
+        if Email.is_include_sobaka(value):
+            raise ValueError('собаку став ) ')
+        self.__email = value
+
+
+c = Email('vfgfg@gmail.com')
+print(c.email)
+c.email = ('fdfdf@ferf.ds')
