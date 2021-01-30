fa3c4bd284eaeda22de5027cd5d27778ff411107
AnatoliyChabanenko <76126324+AnatoliyChabanenko@users.noreply.github.com>
2021-01-30 04:03:51 +0200

    lesson15

--- baza_my.py
+++ baza_my.py
@@ -40,21 +40,28 @@ class Baza:
 
 
 class Podemnik:
-    VOZMOJNUE_VARIANTU = [[Lizh, Helmet], [Sanki], [Bord, Helmet]]
+    VOZMOJNUE_VARIANTU = [(Lizh, Helmet), (Sanki), (Bord, Helmet)]
 
     @staticmethod
     def check_inventar_of_person(inventar):
-        if any(map(lambda x: isinstance(x, Lizh), inventar)):
-            if any(map(lambda x: isinstance(x, Helmet), inventar)):
+        for i in Podemnik.VOZMOJNUE_VARIANTU:
+            if all(map(lambda x: isinstance(x, i), inventar)):
                 return True
-        if any(map(lambda x: isinstance(x, Bord), inventar)):
-            if any(map(lambda x: isinstance(x, Helmet), inventar)):
-                return True
-        if any(map(lambda x: isinstance(x, Sanki), inventar)):
-            return True
+            else:
+                raise ValueError('ей ты что-то забыл')
 
-        else:
-            raise ValueError('ей ты что-то забыл')
+    # @staticmethod
+    # def check_inventar_of_person(inventar):
+    #     if any(map(lambda x: isinstance(x, Lizh), inventar)):
+    #         if any(map(lambda x: isinstance(x, Helmet), inventar)):
+    #             return True
+    #     if any(map(lambda x: isinstance(x, Bord), inventar)):
+    #         if any(map(lambda x: isinstance(x, Helmet), inventar)):
+    #             return True
+    #     if any(map(lambda x: isinstance(x, Sanki), inventar)):
+    #         return True
+    #     else:
+    #         raise ValueError('ей ты что-то забыл')
 
     @staticmethod
     def ride(person: 'Person'):
@@ -72,7 +79,6 @@ class Podemnik:
         y = Podemnik.VOZMOJNUE_VARIANTU[self.index]
         self.index += 1
         return y
-
 
 class Person:
 
@@ -121,14 +127,27 @@ if __name__ == '__main__':
     b1 = Baza('Bukovel')
     l1 = Lizh(343)
     l2 = Helmet(323)
+    h2 = Helmet(434)
+    bo2 = Bord(322)
     b1.add_inventar(l1)
     b1.add_inventar(l2)
+    b1.add_inventar(bo2)
+    b1.add_inventar(h2)
+
 
     print(b1.__dict__)
     p1.take_inventar(b1, l1)
     p1.take_inventar(b1, l2)
-    print(p1.__dict__)
     Podemnik.ride(p1)
     print(p1.__dict__)
     p2 = Person('vasul', 1000)
-    print(l1.__dict__)
+    p2.take_inventar(b1,bo2)
+    p2.take_inventar(b1,h2)
+    Podemnik.ride(p2)
+    p2.retern_inventar(b1,h2)
+
+    print(p2.__dict__)
+
+
+
+
