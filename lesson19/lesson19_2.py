296153d5ec72737c033ed7b9491e3b8d174ec36d
AnatoliyChabanenko <76126324+AnatoliyChabanenko@users.noreply.github.com>
2021-02-06 09:59:22 +0200

    lesson19

---
+++ lesson19/lesson19_2.py
@@ -0,0 +1,26 @@
+
+class FileManager:
+    counter = 0
+
+    @classmethod
+    def get_countetr(cls):
+        return cls.counter
+
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        FileManager.counter += 1
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print(f' менеджер зкарыт уже {self.counter} раз ')
+        self.file.close()
+
+if __name__ == '__main__':
+    pass
+    with FileManager('test_12.txt' ,'r')  as f:
+        print(f.read())
---
+++ lesson19/test_19_2.py
@@ -0,0 +1,32 @@
+from lesson19 import lesson19_2
+import unittest
+
+
+
+class Test_my_class(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.test_fail_manader = lesson19_2.FileManager('test_12.txt', 'r')
+        self.file_name = 'test_12.txt'
+        self.file_mode = 'r'
+
+
+    def test_create(self):
+        test_faile_manader = lesson19_2.FileManager(self.file_name, self.file_mode)
+        self.assertEqual(test_faile_manader.filename,self.file_name)
+        self.assertEqual(test_faile_manader.mode, self.file_mode)
+
+    def test_create_faild(self):
+        test_faile_manader = lesson19_2.FileManager(self.file_name, self.file_mode)
+        self.assertEqual(test_faile_manader.filename, self.file_name)
+        self.assertEqual(test_faile_manader.mode, self.file_mode)
+
+    def test_1(self):
+        with self.test_fail_manader as f:
+            self.assertRaises(SyntaxError , f.read())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
